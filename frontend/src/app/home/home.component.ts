2919bc08e9c4eff3b449fa5116cf09d8475fa4f5
Miriam Grainer <mgrainer@infralovers.com>
2019-01-07 16:23:34 +0100

    index page

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-band',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  /*@Input('image')
+  image = '';
+
+  @Input('name')
+  name = '';
+
+  @Input('description')
+  description = '';*/
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
