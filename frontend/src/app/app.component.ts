decf47f3c19968bd8ce13f6796024a0451a64ab9
Miriam Grainer <miriam.grainer@edu.fh-joanneum.at>
2019-01-08 14:47:35 +0100

    modified login/logout functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {UserService} from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
 
-  constructor(private router: Router) {
+  isLoggedIn: boolean;
+
+  constructor(private router: Router, private userService: UserService) {
+  }
+
+  ngOnInit() {
+    this.isLoggedIn = this.userService.isLoggedIn;
+    this.userService.loggedInChange.subscribe((isLoggedIn) => {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
+
+  logout() {
+    this.userService.logout();
+    this.isLoggedIn = false;
   }
 
   navLogin() {
